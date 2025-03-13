f860952b0ddad8afd283bfb508d51dbccb2c1b43
Rizvan Nanif <joyrizvan@gmail.com>
2025-03-13 19:17:56 -0400

    updated the code with path and config

--- config/config.py
+++ config/config.py
@@ -9,3 +9,9 @@ LLM_LOCAL_URL = "http://10.111.30.94:1234/v1/completions"
 # LLM configuration for AWS Bedrock (example values)
 LLM_BEDROCK_MODEL_ID = "anthropic.claude-v2"
 LLM_BEDROCK_ENDPOINT = "https://bedrock.endpoint.url"
+
+# Titan configuration for local deployment
+TITAN_PROFILE_NAME_RIZVAN = 'rizvan'
+REGION_NAME = 'us-east-1'
+GROQ_API_KEY = "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"
+GROQ_URL = "https://api.groq.com/openai/v1/chat/completions"
